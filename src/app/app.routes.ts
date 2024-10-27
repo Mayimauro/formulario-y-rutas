0572ed129b26914ea64defdc7664ae84497afc75
Matias Mauro <mayimauro1@gmail.com>
2024-10-27 15:34:19 -0300

    TPterminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {RegisterComponent} from './register/register.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {path:'login',component: LoginFormComponent},
+  {path:'home',component: HomePageComponent},
+  {path:'register',component: RegisterComponent},
+];
---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+export class LoginFormComponent {
+
+}
+
